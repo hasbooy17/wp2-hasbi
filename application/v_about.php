ab2b7341576e3ef0302527d73d743a34882491e1
hasbooy17 <hasbifg@gmail.com>
2023-11-14 16:30:36 +0700

    menambhakan v about

---
+++ application/v_about.php
@@ -0,0 +1,24 @@
+<section>
+    <h1><?php echo $judul ?></h1>51
+    <h4>Nama</h4>
+    <ul type="disc">
+        <li>Nama Depan : Hasbi</li>
+        <li>Nama Belakang : Farestu</li>
+    </ul>
+    <br>
+    <h4>Alamat</h4>
+    <ul type="none">
+        <li> Jalan Mekarsari No.45B Bekasi</li>
+    </ul>
+
+    <h4>Tempat Lahir</h4>
+    <ul type="none">
+        <li>Bekasi</li>
+    </ul>
+
+    <h4>Olah Raga Favorit</h4>
+    <ul type="square">
+        <li>Bulutangkis</li>
+        <li>Renang</li>
+    </ul>
+</section>

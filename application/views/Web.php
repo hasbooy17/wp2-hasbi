cae873b8832ae2e58fdfc7cd1d72d03a7d6849ff
hasbooy17 <hasbifg@gmail.com>
2023-11-21 18:00:59 +0700

    menambahkan tugas pertemuan 3,4,5

---
+++ application/views/Web.php
@@ -0,0 +1,7 @@
+public function about()
+    {
+        $data['judul'] = "Halaman About";
+        $this->load->view('v_header', $data);
+        $this->load->view('v_about', $data);
+        $this->load->view('v_footer', $data);
+    }    

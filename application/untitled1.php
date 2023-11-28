78d127f4863c565c62f404f52ec720d317530c75
hasbooy17 <hasbifg17@gmail.com>
2023-11-28 20:48:31 +0700

    menambahkan tugas pertemuan 6 sampai 12

---
+++ application/untitled1.php
@@ -0,0 +1,12 @@
+<?php
+class Contoh1 extends CI_Controller
+{
+
+    public function index()
+    {
+        echo "<h1>Perkenalkan</h1>";
+        echo "Nama saya Hasbi Farestu Gumilar
+              saya tinggal di daerah Bekasi
+              olahraga saya badminton";
+    }
+}
